e9bea3d7c6b37741c1ae5cd1e22d061068d1a6b8
Cecilio Poma <poma6185@gmail.com>
2024-11-27 04:19:46 -0400

    v16

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,6 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     # corsheaders
     "corsheaders.middleware.CorsMiddleware",

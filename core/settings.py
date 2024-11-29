611b72793f7f5cbcf5f98d612a9b35c4877f1050
Cecilio Poma <poma6185@gmail.com>
2024-11-29 02:59:35 -0400

    v16

--- core/settings.py
+++ core/settings.py
@@ -162,10 +162,10 @@ USE_L10N = True  # Averiguar que significa
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
